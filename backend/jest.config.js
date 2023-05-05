cbd7b89dff58631abbe1c2417f4e300558149b54
CozyBrian <dragonbrian33@gmail.com>
2023-05-05 04:04:36 +0000

    added tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ["**/**/*.test.ts"],
   verbose: true,
+  clearMocks: true,
   // forceExit: true,
 };
--- backend/src/__tests__/auth.test.ts
+++ backend/src/__tests__/auth.test.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import * as UserService from "../models/users/user.model";
+import supertest from "supertest";
+import app from "../app";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
 const userId = new mongoose.Types.ObjectId().toString()
 
@@ -12,17 +15,39 @@ const userPayload = {
 const userInput = {
   email: "test@brian.com",
   password: "test@brian",
-  username: "test@brian"
+  username: "brian"
 }
 
 describe("Authentication", () => {
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
+  });
+  
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close(); 
+  });
+  
+
   // User registration
   describe('User registration', () => {
     describe('given the email and password are valid', () => { 
-      it('should return a 201 and token', () => {
+      it('should return a 201 and token', async () => {
         const createUserServiceMock = jest
           .spyOn(UserService, "createNewUser");
 
+        const { statusCode, body } = await supertest(app).post(`/v1/auth/signup`).send(userInput);
+
+        expect(statusCode).toEqual(201);
+
+        expect(body).toHaveProperty("accessToken");
+
+        expect(body).toHaveProperty("refreshToken");
+
+        expect(body).toHaveProperty("expiresIn");
+
+        expect(createUserServiceMock).toHaveBeenCalledWith({...userInput, password: expect.any(String) });
       });
     });
   });
@@ -30,19 +55,31 @@ describe("Authentication", () => {
   describe('User login', () => { 
     describe('given username and password are valid', () => {
       it('should return a signed accessToken & refreshToken', async () => {
-  
+        const { statusCode, body } = await supertest(app).post(`/v1/auth/login`).send(userInput);
+
+        expect(statusCode).toEqual(200);
+
+        expect(body).toHaveProperty("accessToken");
+
+        expect(body).toHaveProperty("refreshToken");
       });
     });
 
     describe('given user not exists', () => { 
       it('should return a 404', async () => {
-  
+        const { statusCode } = await supertest(app).post(`/v1/auth/login`).send({ email: "does@gmail.com", password: "notexists" });
+
+        expect(statusCode).toEqual(404);
       });
     });
 
     describe('given password is incorrect', () => { 
       it('should return a 404', async () => {
-  
+        const { statusCode, body } = await supertest(app).post(`/v1/auth/login`).send({...userInput, password: "notexists" });
+
+        expect(statusCode).toEqual(401);
+
+        expect(body.error).toEqual("WRONG_PASSWORD");
       });
     });
   });
